6d8329e9e3198408fe7ce15e088d0b50f34c54d0
Timothy Cox <tcox@tdkc.com>
2017-06-07 12:41:30 -0400

    2.0

--- vbs.js
+++ vbs.js
@@ -1,19 +1,23 @@
-
+var results = false;
 var goal = 0;
-var total = 0;
 var d1 = 0;
 var d2 = 0;
 var d3 = 0;
 var d4 = 0;
+var current = 0;
+var need = 0;
+var today = 0;
+var total = 0;
 
-var getUrlParameter = function (name) {
+
+var getUrlParameter = function(name) {
     name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
     var regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
     var results = regex.exec(location.search);
     return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
 };
 
-Number.prototype.formatMoney = function(c, d, t){
+Number.prototype.formatMoney = function(c, d, t) {
     var n = this, 
     c = isNaN(c = Math.abs(c)) ? 2 : c, 
     d = d == undefined ? "." : d, 
@@ -22,23 +26,43 @@ Number.prototype.formatMoney = function(c, d, t){
     i = String(parseInt(n = Math.abs(Number(n) || 0).toFixed(c))), 
     j = (j = i.length) > 3 ? j % 3 : 0;
     return s + (j ? i.substr(0, j) + t : "") + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + t) + (c ? d + Math.abs(n - i).toFixed(c).slice(2) : "");
- };
+};
 
 
-var getTotal = function () {
+var reset = function() {
+    
+    
+    total = current;
+    
+
+    // Insert values into DOM
+    $('#goal').text('$' + goal.formatMoney(2));
+    $('#current').text('$' + current.formatMoney(2));
+    $('#need').text('$' + need.formatMoney(2));
+
+    $('#today-container').hide();
+    $('#today').text('');
+
+    $('#total').text(total.formatMoney(2));
+    $('#status').text('');
+};
+
+
+var getTotal = function() {
 
     // Update total
-    var tonightTotal = parseFloat($('#tonight').val());
-    total = d1 + d2 + d3 + d4 + tonightTotal;
+    // var tonightTotal = parseFloat($('#tonight').val());
+    total = current + today;
+    $('#today-container').show();
+    $('#today').text(today.formatMoney(2));
     $('#total').text(total);
 
     // Update status
     setTimeout(function () {
         var status = '';
         if (total < goal) {
-            //var difference = (goal - total).toFixed(2);
-            var difference = (goal - total).formatMoney(2);
-            status = '$' + difference + ' remaining'
+            var difference = goal - total;
+            status = '$' + difference.formatMoney(2) + ' remaining'
         }
         else {
             status = 'MISSION ACCOMPLISHED';
@@ -46,7 +70,19 @@ var getTotal = function () {
         $('#status').text(status);
     }, 3000);
 
+};
+
+var toggle = function() {
+    results = !results;
+    if (results) {
+        getTotal();
+    }
+    else {
+        reset();
+    }
 }
+
+
 
 
 // Get default values from query string
@@ -55,23 +91,24 @@ d1 = getUrlParameter('d1');
 d2 = getUrlParameter('d2');
 d3 = getUrlParameter('d3');
 d4 = getUrlParameter('d4');
+today = getUrlParameter('today');
 
 goal = goal ? parseFloat(goal) : 0;
 d1 = d1 ? parseFloat(d1) : 0;
 d2 = d2 ? parseFloat(d2) : 0;
 d3 = d3 ? parseFloat(d3) : 0;
 d4 = d4 ? parseFloat(d4) : 0;
+today = today ? parseFloat(today) : 0;
 
-total = d1 + d2 + d3 + d4;
+total = current = d1 + d2 + d3 + d4;
+need = goal - current;
 
 
-// Insert values into DOM
-$('#goal').text(goal.formatMoney(2));
-$('#current').text(total.formatMoney(2));
-$('#total').text(total.formatMoney(2));
+reset();
 
 // Register change handler
-$('#tonight').change(getTotal)
+//$('#tonight').change(getTotal)
+$('body').click(toggle)
 
-$('#tonight').focus();
+//$('#tonight').focus();
 
